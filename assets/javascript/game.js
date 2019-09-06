c681f86bda4eed92cc03e09b8feed47383fbb00c
Bailey Beck <baileymbeck@gmail.com>
2019-09-05 20:18:43 -0500

    linked js, made computer choice array and console.log to track correct answer

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+
+//letters for computer's choice
+var computerChoices = ["a"]; 
+//, "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+// "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+    
+//array of variables to match h tags
+    var wins = 0;
+    var losses = 0;
+    var left = 0;
+    var guesses = 9;
+
+//computer random choice + user choice code
+    document.onkeyup = function(event) {
+    var userGuess = event.key;
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+    if(userGuess === computerGuess)
+    console.log(userGuess + computerGuess)
+ }
+
+
+
+
+

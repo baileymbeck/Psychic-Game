33aa8ffef8fa4ce5f886d780d094efd7af79817d
Bailey Beck <baileymbeck@gmail.com>
2019-09-06 14:19:20 -0500

    added js code to adjust win, loss, and console.log answers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,69 @@
 
 //letters for computer's choice
-var computerChoices = ["a"]; 
-//, "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
-// "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+"l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
+
     
 //array of variables to match h tags
     var wins = 0;
     var losses = 0;
-    var left = 0;
-    var guesses = 9;
+    var left = 9;
+    var userguesses = userguesses;
 
-//computer random choice + user choice code
-    document.onkeyup = function(event) {
-    var userGuess = event.key;
+//computer makes one random letter choice 
+//when userguesses computer choice: computer picks new letter, wins++
+//when userguesses a letter: display in list next to userguesses
+    
+    //picks random letter
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-    if(userGuess === computerGuess)
-    console.log(userGuess + computerGuess)
- }
+    document.onkeyup = function(event) {
+    var userGuess = event.key;
+    if(userGuess === computerGuess){
+        console.log(userGuess + computerGuess);
+        wins++;
+        
+     } else if (userGuess !== computerGuess){
+        console.log(userGuess + computerGuess);
+        losses++;
+     } else if (userGuess !== computerGuess){
+        console.log(userGuess + computerGuess);
+        left - left;
+        console.log(userGuess + computerGuess);
+        userGuess;
+     }
+    
+
+    var winsText = document.getElementById("wins");
+    winsText.textContent = wins;
+
+    var lossesText = document.getElementById("losses");
+    lossesText.textContent = losses;
+
+    var leftText = document.getElementById("left");
+    leftText.textContent = left;
+
+    var userguessesText = document.getElementById("userguesses");
+    userguessesText.textContent = userGuess;
+
+    };
+
+
+
+    // var userguessesText = document.getElementById("userguesses");
+    // userguessesText.textContent = userguesses;
+
+
+
+    //document.getElementById(userguesses).textContent(event.key);
+    
+    // for(i = 0; i < computerChoices.length; i++){
+
+    // }
+
+    // if(userGuess === computerGuess){
+    // console.log(userGuess + computerGuess)
+//  }
 
 
 
